98bd7c948109eaa96e26b34e88631036d71f4c26
hrpzcf <hrpzcf@foxmail.com>
2020-11-01 00:42:16 +0800

    refactor: Refactor package structure
    Refactor the package structure to prepare for publishing to PYPI.

---
+++ pyregedit/__init__.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+from .pyregedit import (
+    RegEdit,
+    # 权限设置
+    REG_FLAGS,
+    # root (根节点)
+    HKEY_CLASSES_ROOT,
+    HKEY_CURRENT_USER,
+    HKEY_LOCAL_MACHINE,
+    HKEY_USERS,
+    HKEY_CURRENT_CONFIG,
+    # value type (值类型)
+    REG_SZ,
+    REG_BINARY,
+    REG_DWORD,
+    REG_QWORD,
+    REG_MULTI_SZ,
+    REG_EXPAND_SZ,
+)
+
+__all__ = [
+    "RegEdit",
+    # 权限设置
+    "REG_FLAGS",
+    # root (根节点)
+    "HKEY_CLASSES_ROOT",
+    "HKEY_CURRENT_USER",
+    "HKEY_LOCAL_MACHINE",
+    "HKEY_USERS",
+    "HKEY_CURRENT_CONFIG",
+    # value type (值类型)
+    "REG_SZ",
+    "REG_BINARY",
+    "REG_DWORD",
+    "REG_QWORD",
+    "REG_MULTI_SZ",
+    "REG_EXPAND_SZ",
+]
+
+NAME = "pyregedit"
+VERSION = "0.0.1"
+AUTHOR = "Haddy Yang (杨仕航)"
+WEBSITE = "https://github.com/HaddyYang/pyregedit"
